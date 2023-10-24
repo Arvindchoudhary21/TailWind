1eff29e961d671f1bb63983db9506325ce97236b
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-24 21:45:08 +0530

    card generated

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// uper me /* likha kiuki it will render all the files present in the dist folder
